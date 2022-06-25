028467e52be293e3a1ed9f1bee1bc061613f0dd9
mat0205 <MatheusBispoDev>
2022-06-25 17:50:38 -0300

    Aula 4 completa

---
+++ lib/shared/Auth/auth_controller.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import '../models/user_model.dart';
+
+class AuthController {
+  UserModel? _user;
+
+  UserModel get user => _user!;
+
+  void setUser(BuildContext context, UserModel? user) {
+    if (user != null) {
+      _user = user;
+      
+      saveUser(user: user);
+
+      Navigator.pushReplacementNamed(
+        context,
+        '/home',
+      );
+    } else {
+      Navigator.pushReplacementNamed(
+        context,
+        '/login',
+      );
+    }
+  }
+
+  Future<void> saveUser({required UserModel user}) async {
+    final instance = await SharedPreferences.getInstance();
+    await instance.setString('user', user.toJson());
+  }
+
+  Future<void> currentUser(BuildContext context) async {
+    final instance = await SharedPreferences.getInstance();
+    await Future.delayed(const Duration(seconds: 2));
+    if (instance.containsKey('user')) {
+      final json = instance.getString('user') as String;
+      setUser(context, UserModel.fromJson(json));
+    } else {
+      setUser(context, null);
+    }
+  }
+}
---
+++ lib/shared/widgets/bottom_sheet/bottom_sheet_widget.dart
@@ -0,0 +1,78 @@
+import 'package:flutter/material.dart';
+import 'package:payflow/shared/themes/app_colors.dart';
+import 'package:payflow/shared/widgets/set_label_buttons/set_label_buttons.dart';
+
+import '../../themes/app_text_styles.dart';
+
+class BottomSheetWidget extends StatelessWidget {
+  final String primaryLabel;
+  final Function() primaryOnPressed;
+  final String secondaryLabel;
+  final Function() secondaryOnPressed;
+  final String title;
+  final String subtitle;
+
+  const BottomSheetWidget({
+    Key? key,
+    required this.title,
+    required this.subtitle,
+    required this.primaryLabel,
+    required this.primaryOnPressed,
+    required this.secondaryLabel,
+    required this.secondaryOnPressed,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+      child: RotatedBox(
+        quarterTurns: 1,
+        child: Material(
+          child: Container(
+            color: AppColors.shape,
+            child: Column(
+              children: [
+                Expanded(
+                  child: Container(
+                    color: Colors.black.withOpacity(0.6),
+                  ),
+                ),
+                Column(
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(40.0),
+                      child: Text.rich(
+                        TextSpan(
+                          text: title,
+                          style: TextStyles.buttonBoldHeading,
+                          children: [
+                            TextSpan(
+                              text: '\n$subtitle',
+                              style: TextStyles.buttonHeading,
+                            ),
+                          ],
+                        ),
+                        textAlign: TextAlign.center,
+                      ),
+                    ),
+                    Container(
+                      height: 1,
+                      color: AppColors.stroke,
+                    ),
+                    SetLabelButtons(
+                      enablePrimaryColor: true,
+                      primaryLabel: primaryLabel,
+                      primaryOnPressed: primaryOnPressed,
+                      secondaryLabel: secondaryLabel,
+                      secondaryOnPressed: secondaryOnPressed,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
